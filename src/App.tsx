d774cec7c8c55923a713a3f7019d1db36ea504ab
phil <uniphil@gmail.com>
2025-05-28 13:43:52 -0400

    hello

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class="header">
+        <h1>🛸 UFOs&nbsp;</h1>
+      </div>
+      <div className="card">
+        <p>Samples and stats for every <a href="https://atproto.com/guides/lexicon" target="_blank">lexicon</a> observed in the ATmosphere</p>
+      </div>
+    </>
+  )
+}
+
+export default App
