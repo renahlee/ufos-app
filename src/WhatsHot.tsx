dca8a1018072d79ea8a36e20fb4436470e09173c
phil <uniphil@gmail.com>
2025-05-29 14:06:04 -0400

    nsids and tile things

--- src/WhatsHot.tsx
+++ src/WhatsHot.tsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from 'react'
 import { ButtonGroup } from './components/buttons'
+import { NsidTitle, NsidNice } from './components/nsid'
 import { Fetch } from './fetch'
 import { HostContext } from './context'
 
@@ -64,19 +65,81 @@ export function WhatsHot() {
         using={get_whats_hot}
         args={[host, period]}
         ok={whats_hot => (
-          <ol className="hot">
-            {whats_hot.map(hot => (
-              <li key={hot.current.nsid}>
-                <p>
-                  <strong>{hot.current.nsid}</strong><br/>
-                  {hot.current.dids_estimate} people active<br/>
-                  +{(hot.change * 100).toFixed()}% compared to last week
-                </p>
-              </li>
-            ))}
+          <ol style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '1rem 0.9rem',
+            padding: '0',
+            margin: '0.6rem 0',
+          }}>
+            {whats_hot.map((hot, i) => <Hot key={hot.current.nsid} hot={hot} rank={i+1} />)}
           </ol>
         )}
       />
     </>
+  );
+}
+
+function Hot({ hot, rank }) {
+  return (
+    <li style={{
+      boxSizing: 'border-box',
+      background: 'hsla(0, 0%, 50%, 0.1)',
+      listStyle: 'none',
+      borderRadius: '0.6em',
+      width: '19rem',
+      display: 'flex',
+      gap: '0.3rem',
+      overflow: 'hidden',
+    }}>
+      <span style={{
+        alignSelf: 'top',
+        fontWeight: 'bold',
+        fontSize: '6rem',
+        lineHeight: '6rem',
+        opacity: '0.2',
+        padding: '0rem 0rem',
+        position: 'relative',
+        left: '-0.7rem',
+        marginRight: '-0.7rem',
+        top: '-0.8rem',
+        marginBottom: '-3rem'
+      }}>
+        {rank}
+      </span>
+      <div style={{
+        padding: '0.3rem 0.3rem',
+        width: '100%',
+      }}>
+        <div style={{ marginBottom: '0.4rem'}}>
+          <h3><NsidTitle nsid={hot.current.nsid} />{hot.change > 0.9 && '🔥'}</h3>
+          <p style={{ margin: '0', fontSize: '0.8rem', lineHeight: '0.8rem' }}>
+            <NsidNice nsid={hot.current.nsid} subtle={true} />
+          </p>
+        </div>
+        <p style={{
+          margin: '0',
+          textAlign: 'right',
+          fontSize: '0.8rem',
+          lineHeight: '0.8rem',
+        }}>
+          <strong>{hot.current.dids_estimate}</strong>
+          <span style={{ color: '#ccc' }}> active </span>
+          <span style={{
+            fontSize: '0.8rem',
+            background: 'hsla(0, 0%, 0%, 0.3)',
+            color: hot.change > 0.9
+              ? 'yellow'
+              : hot.change > 0
+                ? '#64ff6c'
+                : 'inherit',
+            borderRadius: '0.3rem',
+            padding: '0 0.2rem 0 0.1rem',
+          }}>
+            {hot.change > 0 && '+'}{(hot.change * 100).toFixed()}%
+          </span>
+        </p>
+      </div>
+    </li>
   );
 }
---
+++ src/components/nsid.tsx
@@ -0,0 +1,46 @@
+import './nsid.css'
+
+const getBits = nsid => {
+  const segments = nsid.split('.');
+
+  const tld = segments.length > 2 ? segments[0] : null; // assumed; todo: psl
+  const app = segments.length > 2 ? segments[1] : segments[0];
+  const mids = segments.length > 4
+    ? segments.slice(2, segments.length - 2).join('.')
+    : null;
+  const group = segments.length > 3 ? segments.at(-2) : null;
+  const name = segments.at(-1);
+  return { tld, app, mids, group, name };
+};
+
+export function NsidTitle({ nsid }) {
+  const bits = getBits(nsid);
+  if (bits.app && bits.name) {
+    return (
+      <span className="nsid-title">
+        <span className="nsid-app">{bits.app}</span>
+        {' '}
+        <span className="nsid-name">{bits.name}</span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="nsid-title">
+        <span className="nsid-name">{bits.name}</span>
+      </span>
+    );
+  }
+}
+
+export function NsidNice({ nsid, subtle }) {
+  const { tld, app, mids, group, name } = getBits(nsid);
+  return (
+    <span className={`nsid-nice ${subtle ? 'subtle' : ''}`}>
+      {tld && (<><span className="nsid-tld">{tld}</span>.</>)}
+      {app && (<><span className="nsid-app">{app}</span>.</>)}
+      {mids && (<><span className="nsid-mids">{mids.join('.')}</span>.</>)}
+      {group && (<><span className="nsid-group">{group}</span>.</>)}
+      {name && (<span className="nsid-name">{name}</span>)}
+    </span>
+  );
+}
