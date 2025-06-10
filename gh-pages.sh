2b2563739564090ec286ae73a38b562da9f9e71e
phil <uniphil@gmail.com>
2025-06-10 18:47:26 -0400

    put the script in the script

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,7 +5,20 @@ set +x
 git switch gh-pages
 git merge --no-ff main -m 'merge main'
 
-npm run gh-pages
+npm run just-build
+cp docs/CNAME dist/
+rm -fr docs
+mv dist docs
+
+mkpage () {
+  local page=$1
+  mkdir -p "docs${page}"
+  cp docs/index.html "docs${page}/index.html"
+}
+
+mkpage /all
+mkpage /collection
+mkpage /status
 
 git add docs
 git commit -m 'update build'
