906d245e6cda9df59f2d1a74efb83954d93de1a5
phil <uniphil@gmail.com>
2025-06-13 11:52:46 -0400

    indicate ghosts on all-collections
    nsids that have no more living records

--- src/pages/all/index.tsx
+++ src/pages/all/index.tsx
@@ -7,6 +7,7 @@ import './all.css';
 
 export function All({}) {
   const [searchActive, setSearchActive] = useState(false);
+  const [showGhosts, setShowGhosts] = useState(false);
 
   return (
     <>
@@ -16,28 +17,39 @@ export function All({}) {
 
       <div className={`all-collections ${searchActive ? 'all-collections-hidden' : ''}`}>
         {/* we don't want to actually remove these from the react tree to avoid re-fetching*/}
-        <h2>All collections</h2>
+        <div className="all-collections-control">
+          <h2>All collections</h2>
+          <label className="">
+            <input type="checkbox" onChange={() => setShowGhosts(h => !h)} checked={showGhosts} />
+            {' '}
+            show ghosts
+          </label>
+        </div>
         <GetJson
           endpoint="/collections"
           params={{limit: 200}}
-          ok={first => <List resp={first} />}
+          ok={first => <List resp={first} showGhosts={showGhosts} />}
         />
       </div>
     </>
   );
 }
 
-function List({ resp }) {
+function List({ resp, showGhosts }) {
   const [showMore, setShowMore] = useState(false);
+
+  const active = showGhosts
+    ? resp.collections
+    : resp.collections.filter(c => c.creates > c.deletes);
 
   return (
     <>
-      {resp.collections.map(c => <CollectionItem c={c} />)}
+      {active.map(c => <CollectionItem c={c} />)}
       {resp.cursor && (showMore
         ? <GetJson
             endpoint="/collections"
             params={{limit: 200, cursor: resp.cursor}}
-            ok={next => <List resp={next} />}
+            ok={next => <List resp={next} showGhosts={showGhosts} />}
           />
         : (
           <p>
@@ -51,14 +63,18 @@ function List({ resp }) {
   );
 }
 
-const CollectionItem = ({ c }) => (
-  <Link
-    key={c.nsid}
-    to={`/collection/?nsid=${c.nsid}`}
-    className="all-collections-item"
-  >
-    <NsidBar n={c.dids_estimate} />
-    &nbsp;
-    <NsidNice nsid={c.nsid} />
-  </Link>
-);
+const CollectionItem = ({ c }) => {
+  let ghosty = (c.deletes >= c.creates) && (c.deletes > 0);
+  return (
+    <Link
+      key={c.nsid}
+      to={`/collection/?nsid=${c.nsid}`}
+      className={`all-collections-item ${ghosty ? 'ghost' : ''}`}
+    >
+      <NsidBar n={c.dids_estimate} />
+      &nbsp;
+      {ghosty && <span className="ghosty">&nbsp;</span>}
+      <NsidNice nsid={c.nsid} />
+    </Link>
+  );
+};
